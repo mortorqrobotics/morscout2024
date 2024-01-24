42e168ba98fd97dedbd2287eddba008834711e72
Scoozy <79184720+scoooozy@users.noreply.github.com>
2024-01-23 22:57:56 -0800

    ah

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import React from "react";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import "./header.css";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
   return (
     <div>
-        <nav>
-            <div >
-                <ArrowBackIcon className='backIcon' style={{fill: " #fff" } }/>
-            </div>
-            <div>
-                <h2>
-                    {props.headerText}
-                </h2>
-            </div>
-        </nav>
+      <nav>
+        <div>
+          <Link to={props.toWhere}>
+            <ArrowBackIcon className="backIcon" style={{ fill: " #fff" }} />
+          </Link>
+        </div>
+        <div>
+          <h2>{props.headerText}</h2>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- frontend/src/pages/pitScoutTeamSelectPage/pitScoutTeamSelectPage.jsx
+++ frontend/src/pages/pitScoutTeamSelectPage/pitScoutTeamSelectPage.jsx
@@ -1,38 +1,41 @@
-import React from 'react'
-import PitButton from '../../components/pitButton/pitButton'
-import Header from '../../components/header/header'
-import { Link } from 'react-router-dom'
+import React from "react";
+import PitButton from "../../components/pitButton/pitButton";
+import Header from "../../components/header/header";
+import { Link } from "react-router-dom";
 const Pitscoutpage = () => {
   return (
     <div>
-      <Header headerText={
-        <> 
-          <span style={{color:"#FF7F23"}}>Pit </span>
-          <span style={{color:"#FFFFFF"}}>Scout</span>
-        </>
-      } />
+      <Header
+        toWhere="/"
+        headerText={
+          <>
+            <span style={{ color: "#FF7F23" }}>Pit </span>
+            <span style={{ color: "#FFFFFF" }}>Scout</span>
+          </>
+        }
+      />
       <Link to="/pit-team-choice/1515">
-        <PitButton teamNum="1515"/>
+        <PitButton teamNum="1515" />
       </Link>
-      
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-      <PitButton teamNum="1515"/>
-    </div>
-  )
-}
 
-export default Pitscoutpage
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+      <PitButton teamNum="1515" />
+    </div>
+  );
+};
+
+export default Pitscoutpage;
