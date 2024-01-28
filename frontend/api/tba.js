f5ecfd3c50f1922cf301d4fa41a6a5985ee7faa8
Scoozy <79184720+scoooozy@users.noreply.github.com>
2024-01-27 19:34:11 -0800

    start working on match scout

--- frontend/api/tba.js
+++ frontend/api/tba.js
@@ -21,3 +21,26 @@ export const getEventTeamsNumbers = async () => {
     }
 
 };
+
+export const getEventMatches = async () => {
+    try {
+        const { data } = await axios.get(
+            `${BASE_URL}/event/${import.meta.env.VITE_EVENT_KEY}/matches/simple`,
+            {
+              headers: {
+                "X-TBA-Auth-Key": import.meta.env.VITE_API_KEY,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          console.log(data)
+          return data.map((match) => ({
+            red_team: match.alliances.red.team_keys,
+            blue_team: match.blue.team_keys,
+            matchNum: match.match_number
+          }));
+    } catch (error) {
+        throw new Error(error)
+    }
+    
+}
--- frontend/src/pages/matchscoutPage/matchscoutPage.jsx
+++ frontend/src/pages/matchscoutPage/matchscoutPage.jsx
@@ -2,8 +2,21 @@ import React from "react";
 import "./matchscoutPage.css";
 import Header from "../../components/header/header";
 import MatchButton from "../../components/matchButton/matchButton";
-
+import { useState, useEffect } from "react";
+import { getEventMatches } from "../../../api/tba";
 const MatchscoutPage = () => {
+  const [teams, setTeams] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    setLoading(true);
+    getEventMatches()
+      .then((data) => setTeams(data))
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => setLoading(false));
+  }, [setLoading]);
   return (
     <div>
       <Header
@@ -16,9 +29,15 @@ const MatchscoutPage = () => {
         }
       />
       <div className="match-buttons">
-        <MatchButton teamNums={[1515,4414,908,1515,1515,1515]}></MatchButton>
-        <MatchButton teamNums={[1515,4414,908,1515,1515,1515]}></MatchButton>
-        <MatchButton teamNums={[1515,4414,908,1515,1515,1515]}></MatchButton>
+        <MatchButton
+          teamNums={[1515, 4414, 908, 1515, 1515, 1515]}
+        ></MatchButton>
+        <MatchButton
+          teamNums={[1515, 4414, 908, 1515, 1515, 1515]}
+        ></MatchButton>
+        <MatchButton
+          teamNums={[1515, 4414, 908, 1515, 1515, 1515]}
+        ></MatchButton>
       </div>
     </div>
   );
