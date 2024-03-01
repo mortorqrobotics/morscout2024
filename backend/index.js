cbf5aa9453fb4f276ced257ccad05f377cb2374f
Scoozy <79184720+scoooozy@users.noreply.github.com>
2024-03-01 13:18:59 -0800

    i love backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
+User
+// server.js
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const db = require("./firebase");
-const excel = require("xlsx");
+const excel = require("xlsx")
 
 dotenv.config();
 const app = express();
@@ -10,89 +12,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Define function to get scout data
-const getScoutData = async (scoutType) => {
-  try {
-    const collection = db.collection(scoutType);
-    const documents = await collection.listDocuments();
-    const documentsData = await Promise.all(
-      documents.map(async (document) => {
-        const documentRef = await document.get();
-        return documentRef.data();
-      })
-    );
-    return documentsData;
-  } catch (error) {
-    console.error("Error fetching scout data:", error);
-    throw error;
-  }
-};
-
-// Endpoint for getting match scout data
-app.get("/get-matchscout-data", async (req, res) => {
-  try {
-    const matchScoutData = await getScoutData("matchscout");
-    res.status(200).json({ success: true, data: matchScoutData });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, error: "Internal Server Error" });
-  }
-});
-
-// Endpoint for getting pit scout data
-app.get("/get-pitscout-data", async (req, res) => {
-  try {
-    const pitScoutData = await getScoutData("pitscout");
-    res.status(200).json({ success: true, data: pitScoutData });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, error: "Internal Server Error" });
-  }
-});
-
-app.get("/download-pitscout-data", async (req, res) => {
-  try {
-    const pitScoutData = await getScoutData("pitscout");
-
-    // Transform the data into an array of objects if necessary
-    const transformedData = pitScoutData.map((document) => ({
-      teamNumber: document.id,
-      ...document.data().pitscout,
-    }));
-
-    // Create Excel workbook and worksheet
-    const workbook = excel.utils.book_new();
-    const worksheet = excel.utils.json_to_sheet(transformedData);
-    excel.utils.book_append_sheet(workbook, worksheet, "PitScouts");
-
-    // Write the workbook to a buffer
-    const buffer = excel.write(workbook, { type: "buffer" });
-
-    // Set the appropriate headers for downloading the file
-    res.setHeader(
-      "Content-Type",
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    );
-    res.setHeader(
-      "Content-Disposition",
-      "attachment; filename=PitScouts.xlsx"
-    );
-
-    // Send the buffer as the response
-    res.send(buffer);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, error: "Internal Server Error" });
-  }
-});
 
 
-
-// Define function to submit scout form
 const submitScoutForm = async (req, res, scoutType, collectionName) => {
   try {
     const { teamNumber } = req.params;
-    const { username, ...formFields } = req.body;
+    const { yourName, ...formFields } = req.body;
 
     // Check if the teamNumber document exists in the specified collection
     const teamDocRef = db.collection(collectionName).doc(teamNumber);
@@ -104,7 +29,7 @@ const submitScoutForm = async (req, res, scoutType, collectionName) => {
       // Create a new submission with the person's name
       const newSubmissionKey = `submission${Object.keys(scoutData).length + 1}`;
       const newSubmission = {
-        [username]: formFields,
+        [yourName]: formFields,
       };
 
       // Append the new submission to the existing scout data
@@ -115,7 +40,7 @@ const submitScoutForm = async (req, res, scoutType, collectionName) => {
       // If the document doesn't exist, create a new one
       const initialData = {
         [`submission1`]: {
-          [username]: formFields,
+          [yourName]: formFields,
         },
       };
 
@@ -148,7 +73,42 @@ app.post("/submit-pitscout/:teamNumber", async (req, res) => {
   submitScoutForm(req, res, "pitscout", "pitscout");
 });
 
-// Start server
+app.get("/", async (req, res) => {
+  const collection = db.collection("pitscout");
+  const documents = await collection.listDocuments();
+  const documentsData  = await Promise.all(
+    documents.map(async (document) => {
+      const documentRef = await document.get();
+      const pitscout = documentRef.data().pitscout;
+      const output = []
+      for(let submissionKey in pitscout) {
+        const pitscoutData = pitscout[submissionKey]
+        const person = pitscoutData[Object.keys(pitscoutData)[0]]
+
+        output.push( {
+          teamNumber: document.id,
+          name: Object.keys(pitscoutData)[0],
+          ...person
+        })
+
+      }
+
+      return output
+    })
+  )
+  const data = [ ]
+  for(let personCollection of documentsData) {
+    for(let person of personCollection) {
+      data.push(person)
+    }
+  }
+  const workbook = excel.utils.book_new()
+  const worksheet = excel.utils.json_to_sheet(data);
+  excel.utils.book_append_sheet(workbook, worksheet, 'Sheet 1')
+  excel.writeFile(workbook, 'PitScouts.xlsx')
+    return res.json(data);
+})
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
